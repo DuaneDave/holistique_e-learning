4063fa01d8f3d48215a9e66cd449df2956c5f7a0
duanedave <duanedave17@gmail.com>
2023-06-22 19:52:26 +0100

    Render user routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import db from './config/db.js';
+import allowedOrigins from './config/origin.js';
+import apiRouter from './routes/api.routes.js';
 
 db();
 
@@ -9,11 +13,10 @@ const app = express();
 const PORT = 4000;
 
 app.use(express.json());
+app.use(cors(allowedOrigins()));
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  console.log('GET /');
-  res.send('Hello World');
-});
+app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
