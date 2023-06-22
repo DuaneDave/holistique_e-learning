35a14d52a731475780696b736039e6fb89cc21b2
duanedave <duanedave17@gmail.com>
2023-06-22 19:54:38 +0100

    Create use routes

---
+++ server/routes/user.routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import { signup, login } from '../controllers/user.controller.js';
+
+const userRouter = Router();
+
+userRouter.post('/signup', signup);
+userRouter.post('/login', login);
+
+export default userRouter;
