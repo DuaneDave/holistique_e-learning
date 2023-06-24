3393da1c2a59aafc09ff4e72c9b3628cba8e67ce
duanedave <duanedave17@gmail.com>
2023-06-24 22:48:57 +0100

    Create the user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -10,13 +10,22 @@ export const signup = async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
+    const isExisting = await User.findOne({ email });
+
+    if (isExisting) {
+      console.log(isExisting);
+      return res
+        .status(400)
+        .json({ message: 'User already exists', status: 400 });
+    }
+
     User.create({
       username,
       email,
       password: bcrypt.hashSync(password, salt),
     });
 
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({ message: 'User created successfully', status: 201 });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -26,16 +35,20 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const foundUser = User.findOne({ email });
+    const foundUser = await User.findOne({ email });
 
     if (!foundUser) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res
+        .status(400)
+        .json({ message: 'Invalid credentials', status: 400 });
     }
 
     const isPasswordCorrect = bcrypt.compareSync(password, foundUser.password);
 
     if (!isPasswordCorrect) {
-      return res.status(400).json({ message: 'Invalid credentials' });
+      return res
+        .status(400)
+        .json({ message: 'Invalid credentials', status: 400 });
     }
 
     jwt.sign(
@@ -44,7 +57,8 @@ export const login = async (req, res) => {
       {},
       (error, token) => {
         if (error) {
-          return res.status(500).json({ message: error.message });
+          console.log(error);
+          return res.status(500).json({ message: error.message, status: 500 });
         }
 
         res
@@ -62,6 +76,6 @@ export const login = async (req, res) => {
       }
     );
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message, status: 500 });
   }
 };
