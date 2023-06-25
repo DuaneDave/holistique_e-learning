cc90c757a4f544dab70b5dd294a874e92739cc5f
duanedave <duanedave17@gmail.com>
2023-06-25 14:58:39 +0100

    Create dropdown component for the header

---
+++ client/component/layout/Dropdown.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useState, useContext } from 'react';
+import Link from 'next/link';
+
+import { AuthContext } from '@/store/authContext';
+
+import styles from './header.module.css';
+
+function Dropdown({ ...props }) {
+  const { user, logout } = useContext(AuthContext);
+
+  return (
+    <div className={`flex flex-col gap ${styles.dropdown}`} {...props}>
+      <Link href="/profile">My Profile</Link>
+      <button onClick={() => logout()}>Logout</button>
+    </div>
+  );
+}
+
+export default Dropdown;
