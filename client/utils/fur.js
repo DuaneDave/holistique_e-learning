f6b02311595ee7d93be12411c2df66c9aa757a38
duanedave <duanedave17@gmail.com>
2023-06-26 14:57:28 +0100

    Add utility for fur

---
+++ client/utils/fur.js
@@ -0,0 +1,28 @@
+import image1 from '@public/assets/images/usedRes1.png';
+import image2 from '@public/assets/images/usedRes2.png';
+
+const frequentlyUsedResources = [
+  {
+    id: 1,
+    title: 'integer id Orc Sed Ante incidunt',
+    image: image1,
+    content:
+      'Cras convalis lacus orci, tristque tincidunt magna fringila at faucibus vel.',
+  },
+  {
+    id: 2,
+    title: 'integer id Orc Sed Ante incidunt',
+    image: image2,
+    content:
+      'Cras convalis lacus orci, tristque tincidunt magna fringila at faucibus vel.',
+  },
+  {
+    id: 3,
+    title: 'integer id Orc Sed Ante incidunt',
+    image: image1,
+    content:
+      'Cras convalis lacus orci, tristque tincidunt magna fringila at faucibus vel.',
+  },
+];
+
+export default frequentlyUsedResources;
