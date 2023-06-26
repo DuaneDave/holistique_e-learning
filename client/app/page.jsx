ddfa14fff36469f00944e8d0f0302b7f64eab1a2
duanedave <duanedave17@gmail.com>
2023-06-26 14:58:13 +0100

    Add and render the fur component

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -2,12 +2,14 @@ import styles from './page.module.css';
 
 import HeroSection from '@/component/homepage/HeroSection';
 import CardSection from '@/component/homepage/CardSection';
+import FUR from '@/component/homepage/FUR';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <CardSection />
+      <FUR />
     </>
   );
 }
