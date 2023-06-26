a444c49247bcc57c75a1090abb0343b591d9b6ab
duanedave <duanedave17@gmail.com>
2023-06-26 09:03:28 +0100

    Render the card section

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,11 +1,13 @@
 import styles from './page.module.css';
 
 import HeroSection from '@/component/homepage/HeroSection';
+import CardSection from '@/component/homepage/CardSection';
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <CardSection />
     </>
   );
 }
