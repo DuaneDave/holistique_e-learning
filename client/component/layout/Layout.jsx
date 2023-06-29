c1e16e2b4518ce36fa45988519c6e8146e880783
Lena <summyalena@gmail.com>
2023-06-29 18:20:44 +0100

    Add authentication for newsletter

---
+++ client/component/layout/Layout.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+
+
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+function Layout({children}) {
+  return (
+    <>
+    <Header/>
+    <main>
+     {children}
+     </main>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
