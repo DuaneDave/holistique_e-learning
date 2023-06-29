3110c1a0deac31bf4f9c920c8413e382118c9d39
Lena <summyalena@gmail.com>
2023-06-29 18:20:05 +0100

    Create footer

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -5,15 +5,18 @@ import CardSection from '@/component/homepage/CardSection';
 import FURSection from '@/component/homepage/FURSection';
 import CtaSection from '@/component/homepage/CtaSection';
 import QuoteSection from '@/component/homepage/QuoteSection';
+import Layout from '@/component/layout/Layout';
 
 export default function Home() {
   return (
     <>
+    <Layout>
       <HeroSection />
       <CardSection />
       <FURSection />
       <CtaSection />
       <QuoteSection />
+      </Layout>
     </>
   );
 }
