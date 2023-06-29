a03e5e61925ff2a2fdf70730c784c4ab124c2775
duanedave <duanedave17@gmail.com>
2023-06-29 20:09:39 +0100

    Add resources utility

---
+++ client/utils/resources.js
@@ -0,0 +1,54 @@
+import resource1 from '@public/assets/images/resource1.png';
+import resource2 from '@public/assets/images/resource2.png';
+import resource3 from '@public/assets/images/resource3.png';
+import resource4 from '@public/assets/images/resource4.png';
+import resource5 from '@public/assets/images/resource5.png';
+import resource6 from '@public/assets/images/resource6.png';
+
+const resources = [
+  {
+    id: 1,
+    title: 'Skin and Soft Tissue Infection',
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Nibh id ut egestas nam. Eget arcu commodo consectetur ultrices aliquam habitant posuere aliquet. Ipsum porttitor pretium pretium laoreet mauris elementum integer volutpat. Scelerisque sit donec metus senectus enim id in. Sed in cras imperdiet non. Id ultricies erat viverra molestie. Quis pellentesque fermentum nullam in tortor nam. Est elit feugiat pulvinar in eu viverra donec. Sed etiam amet egestas felis lobortis turpis dictum. Nisl pellentesque rhoncus sit etiam. Ornare sed metus sit sit. Non non mauris adipiscing massa.',
+    image: resource1,
+    link: 'healtcare.org'
+  },
+  {
+    id: 2,
+    title: 'Stigma and Discrimination in Healthcare ',
+    description:  'Lorem ipsum dolor sit amet consectetur. Nibh id ut egestas nam. Eget arcu commodo consectetur ultrices aliquam habitant posuere aliquet. Ipsum porttitor pretium pretium laoreet mauris elementum integer volutpat. Scelerisque sit donec metus senectus enim id in. Sed in cras imperdiet non. Id ultricies erat viverra molestie. Quis pellentesque fermentum nullam in tortor nam. Est elit feugiat pulvinar in eu viverra donec. Sed etiam amet egestas felis lobortis turpis dictum. Nisl pellentesque rhoncus sit etiam. Ornare sed metus sit sit. Non non mauris adipiscing massa.',
+    image: resource2,
+    link: 'healtcare.org'
+  },
+  {
+    id: 3,
+    title: 'Harm Reduction',
+    description:  'Lorem ipsum dolor sit amet consectetur. Nibh id ut egestas nam. Eget arcu commodo consectetur ultrices aliquam habitant posuere aliquet. Ipsum porttitor pretium pretium laoreet mauris elementum integer volutpat. Scelerisque sit donec metus senectus enim id in. Sed in cras imperdiet non. Id ultricies erat viverra molestie. Quis pellentesque fermentum nullam in tortor nam. Est elit feugiat pulvinar in eu viverra donec. Sed etiam amet egestas felis lobortis turpis dictum. Nisl pellentesque rhoncus sit etiam. Ornare sed metus sit sit. Non non mauris adipiscing massa.',
+    image: resource3,
+    link: 'healtcare.org'
+  },
+  {
+    id: 4,
+    title: 'Stigma Reduction ',
+    description:  'Lorem ipsum dolor sit amet consectetur. Nibh id ut egestas nam. Eget arcu commodo consectetur ultrices aliquam habitant posuere aliquet. Ipsum porttitor pretium pretium laoreet mauris elementum integer volutpat. Scelerisque sit donec metus senectus enim id in. Sed in cras imperdiet non. Id ultricies erat viverra molestie. Quis pellentesque fermentum nullam in tortor nam. Est elit feugiat pulvinar in eu viverra donec. Sed etiam amet egestas felis lobortis turpis dictum. Nisl pellentesque rhoncus sit etiam. Ornare sed metus sit sit. Non non mauris adipiscing massa.',
+    image: resource4,
+    link: 'healtcare.org'
+  },
+  {
+    id: 5,
+    title: 'Harms Caused From Drug Injection',
+    description:  'Lorem ipsum dolor sit amet consectetur. Nibh id ut egestas nam. Eget arcu commodo consectetur ultrices aliquam habitant posuere aliquet. Ipsum porttitor pretium pretium laoreet mauris elementum integer volutpat. Scelerisque sit donec metus senectus enim id in. Sed in cras imperdiet non. Id ultricies erat viverra molestie. Quis pellentesque fermentum nullam in tortor nam. Est elit feugiat pulvinar in eu viverra donec. Sed etiam amet egestas felis lobortis turpis dictum. Nisl pellentesque rhoncus sit etiam. Ornare sed metus sit sit. Non non mauris adipiscing massa.',
+    image: resource5,
+    link: 'healtcare.org'
+  },
+  {
+    id: 6,
+    title: 'Stigma and Discrimination in Healthcare ',
+    description:  'Lorem ipsum dolor sit amet consectetur. Nibh id ut egestas nam. Eget arcu commodo consectetur ultrices aliquam habitant posuere aliquet. Ipsum porttitor pretium pretium laoreet mauris elementum integer volutpat. Scelerisque sit donec metus senectus enim id in. Sed in cras imperdiet non. Id ultricies erat viverra molestie. Quis pellentesque fermentum nullam in tortor nam. Est elit feugiat pulvinar in eu viverra donec. Sed etiam amet egestas felis lobortis turpis dictum. Nisl pellentesque rhoncus sit etiam. Ornare sed metus sit sit. Non non mauris adipiscing massa.',
+    image: resource6,
+    link: 'healtcare.org'
+  },
+];
+
+export default resources;
