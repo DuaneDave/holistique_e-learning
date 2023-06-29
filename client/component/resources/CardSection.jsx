0c7a6b225f40edb4b0fd3816a0ec83b6c6679e0d
duanedave <duanedave17@gmail.com>
2023-06-29 20:10:40 +0100

    Create the resources card section

---
+++ client/component/resources/CardSection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from 'next/image';
+
+import resources from '@/utils/resources';
+import { Card } from '../ui/Cards';
+
+import styles from './resources.module.css';
+
+function CardSection() {
+  return (
+    <section className={`container grid gap-lg ${styles.resources}`}>
+      {resources.map((resource) => (
+        <Card key={resource.id} className={styles.card}>
+          <Image
+            src={resource.image}
+            alt={resource.title}
+            title={resource.title}
+            className="full-width full-height"
+          />
+
+          <span>
+            <a href={resource.link}>{resource.link}</a>
+          </span>
+
+          <div className={`flex flex-col ${styles.cardContent}`}>
+            <h3 className="text-center">{resource.title}</h3>
+            <p>{resource.description}</p>
+            <button>Learn more</button>
+          </div>
+        </Card>
+      ))}
+    </section>
+  );
+}
+
+export default CardSection;
