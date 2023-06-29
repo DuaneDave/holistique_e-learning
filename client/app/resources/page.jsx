4192d87a6d0e24ef9ac3fd0394e6467b0104bd89
duanedave <duanedave17@gmail.com>
2023-06-29 20:11:01 +0100

    Render the resources components

---
+++ client/app/resources/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HeroSection from '@/component/resources/HeroSection';
+import CardSection from '@/component/resources/CardSection';
+
+function page() {
+  return (
+    <>
+      <HeroSection />
+      <CardSection />
+    </>
+  );
+}
+
+export default page;
