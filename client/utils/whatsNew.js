b19ff192dbcd101d31d82f544e018586a5bf5325
duanedave <duanedave17@gmail.com>
2023-06-29 16:08:12 +0100

    Add a description componet

---
+++ client/utils/whatsNew.js
@@ -0,0 +1,31 @@
+import whatsNew1 from '@public/assets/images/whatsnew1.png';
+import whatsNew2 from '@public/assets/images/whatsnew2.png';
+
+const whatsNew = [
+  {
+    id: 1,
+    title: 'Lorem Ipsum cantona sca vin tu di maszst fakar zarama narida sati',
+    image: whatsNew1,
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Lorem nulla massa cursus nunc elit mauris malesuada faucibus morbi.',
+    views: '251,232',
+  },
+  {
+    id: 2,
+    title: 'Lorem Ipsum cantona sca vin tu di maszst fakar zarama narida sati',
+    image: whatsNew2,
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Lorem nulla massa cursus nunc elit mauris malesuada faucibus morbi.',
+    views: '251,232',
+  },
+  {
+    id: 3,
+    title: 'Lorem Ipsum cantona sca vin tu di maszst fakar zarama narida sati',
+    image: whatsNew1,
+    description:
+      'Lorem ipsum dolor sit amet consectetur. Lorem nulla massa cursus nunc elit mauris malesuada faucibus morbi.',
+    views: '251,232',
+  },
+];
+
+export default whatsNew;
