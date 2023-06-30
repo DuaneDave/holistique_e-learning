6f77a2e0bfa4789f895668eb867b42e76c323a29
Lena <summyalena@gmail.com>
2023-06-30 12:46:43 +0100

    Make changes to footer

--- client/component/layout/Footer.jsx
+++ client/component/layout/Footer.jsx
@@ -1,23 +1,31 @@
+'use client';
+
 import Image from 'next/image'
 import React from 'react'
+import { usePathname } from 'next/navigation';
 
 import FooterLogo from '../../public/assets/images/footerIcon.svg';
 import styles from './footer.module.css';
 import NewsLetter from '../auth/newsLetter';
 
 function Footer() {
+  const path = usePathname();
+
+  if (path === '/login' || path === '/signup') {
+    return null;
+  }
   return (
     <footer className={`full-width flex flex-col center gap-md ${styles.footer}`}>
        <div className={`flex center gap ${styles.info}`}>
-        <Image className='flex center' src={FooterLogo} alt="footer_logo" />
+        <Image className='flex center circle' src={FooterLogo} alt="footer_logo" />
         <p>Live Chat with professionals</p>
        </div>
        <div className={`flex flex-col center ${styles.newsLetter}`}>
-       <h1>STAY INFORMED</h1>
+       <h2>STAY INFORMED</h2>
        <p>stay up to date with our initiatives, new resources and project developments</p>
       <NewsLetter/>
       </div>
-      <ul className='flex gap center'>
+      <ul className='flex gap full-width center'>
         <li>Courses</li>
         <li>Privacy Policy</li>
         <li>Terms & Conditions</li>
