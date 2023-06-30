2fd879f950eacb096be613cf5866360f4b532fe5
duanedave <duanedave17@gmail.com>
2023-06-30 16:28:39 +0100

    Render the available courses section

--- client/app/courses/page.jsx
+++ client/app/courses/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import MyCourses from '@/component/courses/MyCourses';
+import AvailableCourses from '@/component/courses/AvailableCourses';
 
 function Courses() {
   return (
     <>
       <MyCourses />
+      <AvailableCourses />
     </>
   );
 }
