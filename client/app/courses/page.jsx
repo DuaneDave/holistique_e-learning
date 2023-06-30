54d02058ca396eab4042d75f9b5dce12d66bbd5d
duanedave <duanedave17@gmail.com>
2023-06-30 17:13:21 +0100

    Render the faq section

--- client/app/courses/page.jsx
+++ client/app/courses/page.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import MyCourses from '@/component/courses/MyCourses';
 import AvailableCourses from '@/component/courses/AvailableCourses';
+import Faq from '@/component/courses/Faq';
 
 function Courses() {
   return (
     <>
       <MyCourses />
       <AvailableCourses />
+      <Faq />
     </>
   );
 }
