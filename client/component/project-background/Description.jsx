4f3aa57656614f4694bb00409754bb202e3c7bed
duanedave <duanedave17@gmail.com>
2023-06-30 17:28:16 +0100

    Fix issues with container class

--- client/component/project-background/Description.jsx
+++ client/component/project-background/Description.jsx
@@ -7,39 +7,42 @@ import styles from './project-background.module.css';
 
 function Description() {
   return (
-    <section className={`container ${styles.description}`}>
-      <Image
-        src={design}
-        alt="heading style"
-        aria-hidden="true"
-        width={80}
-        height={80}
-      />
+    <section>
+      <div className={`container ${styles.description}`}>
+        <Image
+          src={design}
+          alt="heading style"
+          aria-hidden="true"
+          width={80}
+          height={80}
+        />
 
-      <h1>Project Holistique</h1>
+        <h1>Project Holistique</h1>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur. Vitae mauris at scelerisque eu
-        integer. Maecenas turpis enim leo fringilla in massa. Consequat aenean
-        viverra fringilla aliquam duis. Non vel tempor ac nisl duis faucibus sit
-        lacinia. Ac quam scelerisque adipiscing hendrerit vitae sed pellentesque
-        imperdiet aenean. Ultricies facilisis imperdiet tincidunt sollicitudin
-        felis nisl hendrerit. Nisi scelerisque blandit id consectetur lobortis
-        cursus. Ipsum feugiat dui in molestie aliquet. Eu odio tellus integer
-        orci ultricies integer aliquet scelerisque suspendisse. Tincidunt augue
-        dignissim sed neque sit aliquet blandit. Malesuada tempor fermentum nunc
-        nibh metus ullamcorper dui. Nulla nulla nunc scelerisque neque mauris.
-        Sit nibh rhoncus scelerisque ac tellus nisl arcu nibh velit. In proin
-        feugiat id sagittis eget sit interdum. Elementum amet lacus nunc egestas
-        interdum. Ullamcorper non ullamcorper congue consequat mollis est
-        habitasse. Est nunc nam tortor cras. Maecenas enim bibendum in tincidunt
-        tincidunt. Eu ultrices viverra ipsum cras. Facilisis nunc pulvinar ac
-        sed aliquam faucibus. Eu viverra eget egestas id eu eget fringilla.
-        Aenean mattis donec viverra sed laoreet bibendum malesuada ultricies.
-        Pellentesque venenatis eget pharetra scelerisque blandit laoreet lectus.
-        In consequat quis cursus et faucibus tempor turpis.
-      </p>
-      <hr />
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Vitae mauris at scelerisque eu
+          integer. Maecenas turpis enim leo fringilla in massa. Consequat aenean
+          viverra fringilla aliquam duis. Non vel tempor ac nisl duis faucibus
+          sit lacinia. Ac quam scelerisque adipiscing hendrerit vitae sed
+          pellentesque imperdiet aenean. Ultricies facilisis imperdiet tincidunt
+          sollicitudin felis nisl hendrerit. Nisi scelerisque blandit id
+          consectetur lobortis cursus. Ipsum feugiat dui in molestie aliquet. Eu
+          odio tellus integer orci ultricies integer aliquet scelerisque
+          suspendisse. Tincidunt augue dignissim sed neque sit aliquet blandit.
+          Malesuada tempor fermentum nunc nibh metus ullamcorper dui. Nulla
+          nulla nunc scelerisque neque mauris. Sit nibh rhoncus scelerisque ac
+          tellus nisl arcu nibh velit. In proin feugiat id sagittis eget sit
+          interdum. Elementum amet lacus nunc egestas interdum. Ullamcorper non
+          ullamcorper congue consequat mollis est habitasse. Est nunc nam tortor
+          cras. Maecenas enim bibendum in tincidunt tincidunt. Eu ultrices
+          viverra ipsum cras. Facilisis nunc pulvinar ac sed aliquam faucibus.
+          Eu viverra eget egestas id eu eget fringilla. Aenean mattis donec
+          viverra sed laoreet bibendum malesuada ultricies. Pellentesque
+          venenatis eget pharetra scelerisque blandit laoreet lectus. In
+          consequat quis cursus et faucibus tempor turpis.
+        </p>
+        <hr />
+      </div>
     </section>
   );
 }
--- client/component/resources/CardSection.jsx
+++ client/component/resources/CardSection.jsx
@@ -8,27 +8,29 @@ import styles from './resources.module.css';
 
 function CardSection() {
   return (
-    <section className={`container grid gap-lg ${styles.resources}`}>
-      {resources.map((resource) => (
-        <Card key={resource.id} className={styles.card}>
-          <Image
-            src={resource.image}
-            alt={resource.title}
-            title={resource.title}
-            className="full-width full-height"
-          />
+    <section>
+      <div className={`container grid gap-lg ${styles.resources}`}>
+        {resources.map((resource) => (
+          <Card key={resource.id} className={styles.card}>
+            <Image
+              src={resource.image}
+              alt={resource.title}
+              title={resource.title}
+              className="full-width full-height"
+            />
 
-          <span>
-            <a href={resource.link}>{resource.link}</a>
-          </span>
+            <span>
+              <a href={resource.link}>{resource.link}</a>
+            </span>
 
-          <div className={`flex flex-col ${styles.cardContent}`}>
-            <h3 className="text-center">{resource.title}</h3>
-            <p>{resource.description}</p>
-            <button>Learn more</button>
-          </div>
-        </Card>
-      ))}
+            <div className={`flex flex-col ${styles.cardContent}`}>
+              <h3 className="text-center">{resource.title}</h3>
+              <p>{resource.description}</p>
+              <button>Learn more</button>
+            </div>
+          </Card>
+        ))}
+      </div>
     </section>
   );
 }
