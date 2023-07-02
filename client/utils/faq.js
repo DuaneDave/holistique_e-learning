1d9351f1c181c727e009c3ad0a782ce473cce38e
duanedave <duanedave17@gmail.com>
2023-07-02 19:46:02 +0100

    Create the faqs

---
+++ client/utils/faq.js
@@ -0,0 +1,53 @@
+const faqs = [
+  {
+    id: 1,
+    question: 'How can i manage stigma?',
+    answer: `
+    Lorem ipsum dolor sit amet consectetur. Pretium est at consectetur odio neque lobortis justo eu fusce. Mi ipsum in interdum ut scelerisque amet elit ut. Scelerisque facilisis non orci metus ornare. At luctus eu morbi sollicitudin amet in elementum accumsan. Sed mollis sagittis eleifend gravida. Feugiat feugiat facilisis pellentesque consequat placerat etiam vitae turpis. Cras nunc massa gravida potenti. In euismod in senectus dictum quam quam urna ac. Sem vel faucibus morbi elementum urna.
+    Vitae ipsum malesuada malesuada blandit metus mauris purus leo. Facilisi porttitor dolor amet adipiscing ultricies nunc in. Lacinia tempus placerat lorem aliquam. Eu sed quis pellentesque semper at tellus. Ut libero a iaculis facilisis quis lectus libero amet. Volutpat amet accumsan velit nisl et gravida augue accumsan lobortis. Pharetra sed sed metus eu at tristique in fermentum lacus. Facilisis mattis adipiscing interdum venenatis donec elementum id justo. Elementum nullam sed felis in massa sem volutpat sit. Ut aliquam enim at arcu aliquam nam adipiscing. Ornare amet volutpat eget rhoncus pellentesque consectetur eget purus at. Vulputate morbi et at velit sapien.
+    Ut egestas leo proin pellentesque vitae morbi. Sed ac risus ac ullamcorper nunc pharetra nulla fames vulputate. Vel quam elementum congue mauris tempor massa pellentesque nunc. Tincidunt mauris arcu sed pellentesque vulputate posuere eu. Accumsan ut condimentum ultricies lorem enim aliquet suspendisse. Eu ipsum elit condimentum urna dui vulputate volutpat vestibulum.
+    `,
+  },
+  {
+    id: 2,
+    question: 'Can i stop self medication?',
+    answer: `
+    Lorem ipsum dolor sit amet consectetur. Pretium est at consectetur odio neque lobortis justo eu fusce. Mi ipsum in interdum ut scelerisque amet elit ut. Scelerisque facilisis non orci metus ornare. At luctus eu morbi sollicitudin amet in elementum accumsan. Sed mollis sagittis eleifend gravida. Feugiat feugiat facilisis pellentesque consequat placerat etiam vitae turpis. Cras nunc massa gravida potenti. In euismod in senectus dictum quam quam urna ac. Sem vel faucibus morbi elementum urna.
+    Vitae ipsum malesuada malesuada blandit metus mauris purus leo. Facilisi porttitor dolor amet adipiscing ultricies nunc in. Lacinia tempus placerat lorem aliquam. Eu sed quis pellentesque semper at tellus. Ut libero a iaculis facilisis quis lectus libero amet. Volutpat amet accumsan velit nisl et gravida augue accumsan lobortis. Pharetra sed sed metus eu at tristique in fermentum lacus. Facilisis mattis adipiscing interdum venenatis donec elementum id justo. Elementum nullam sed felis in massa sem volutpat sit. Ut aliquam enim at arcu aliquam nam adipiscing. Ornare amet volutpat eget rhoncus pellentesque consectetur eget purus at. Vulputate morbi et at velit sapien.
+    `,
+  },
+  {
+    id: 3,
+    question: 'What are the dangers of injecting drugs?',
+    answer: `
+    Lorem ipsum dolor sit amet consectetur. Pretium est at consectetur odio neque lobortis justo eu fusce. Mi ipsum in interdum ut scelerisque amet elit ut. Scelerisque facilisis non orci metus ornare. At luctus eu morbi sollicitudin amet in elementum accumsan. Sed mollis sagittis eleifend gravida. Feugiat feugiat facilisis pellentesque consequat placerat etiam vitae turpis. Cras nunc massa gravida potenti. In euismod in senectus dictum quam quam urna ac. Sem vel faucibus morbi elementum urna.
+    Vitae ipsum malesuada malesuada blandit metus mauris purus leo. Facilisi porttitor dolor amet adipiscing ultricies nunc in. Lacinia tempus placerat lorem aliquam. Eu sed quis pellentesque semper at tellus. Ut libero a iaculis facilisis quis lectus libero amet. Volutpat amet accumsan velit nisl et gravida augue accumsan lobortis. Pharetra sed sed metus eu at tristique in fermentum lacus. Facilisis mattis adipiscing interdum venenatis donec elementum id justo. Elementum nullam sed felis in massa sem volutpat sit. Ut aliquam enim at arcu aliquam nam adipiscing. Ornare amet volutpat eget rhoncus pellentesque consectetur eget purus at. Vulputate morbi et at velit sapien.
+    `,
+  },
+  {
+    id: 5,
+    question: 'How do people live with drug users?',
+    answer: `
+    Lorem ipsum dolor sit amet consectetur. Pretium est at consectetur odio neque lobortis justo eu fusce. Mi ipsum in interdum ut scelerisque amet elit ut. Scelerisque facilisis non orci metus ornare. At luctus eu morbi sollicitudin amet in elementum accumsan. Sed mollis sagittis eleifend gravida. Feugiat feugiat facilisis pellentesque consequat placerat etiam vitae turpis. Cras nunc massa gravida potenti. In euismod in senectus dictum quam quam urna ac. Sem vel faucibus morbi elementum urna.
+    Vitae ipsum malesuada malesuada blandit metus mauris purus leo. Facilisi porttitor dolor amet adipiscing ultricies nunc in. Lacinia tempus placerat lorem aliquam. Eu sed quis pellentesque semper at tellus. Ut libero a iaculis facilisis quis lectus libero amet. Volutpat amet accumsan velit nisl et gravida augue accumsan lobortis. Pharetra sed sed metus eu at tristique in fermentum lacus. Facilisis mattis adipiscing interdum venenatis donec elementum id justo. Elementum nullam sed felis in massa sem volutpat sit. Ut aliquam enim at arcu aliquam nam adipiscing. Ornare amet volutpat eget rhoncus pellentesque consectetur eget purus at. Vulputate morbi et at velit sapien.
+    `,
+  },
+  {
+    id: 6,
+    question: 'How do people live with drug users?',
+    answer: `
+    Lorem ipsum dolor sit amet consectetur. Pretium est at consectetur odio neque lobortis justo eu fusce. Mi ipsum in interdum ut scelerisque amet elit ut. Scelerisque facilisis non orci metus ornare. At luctus eu morbi sollicitudin amet in elementum accumsan. Sed mollis sagittis eleifend gravida. Feugiat feugiat facilisis pellentesque consequat placerat etiam vitae turpis. Cras nunc massa gravida potenti. In euismod in senectus dictum quam quam urna ac. Sem vel faucibus morbi elementum urna.
+    Vitae ipsum malesuada malesuada blandit metus mauris purus leo. Facilisi porttitor dolor amet adipiscing ultricies nunc in. Lacinia tempus placerat lorem aliquam. Eu sed quis pellentesque semper at tellus. Ut libero a iaculis facilisis quis lectus libero amet. Volutpat amet accumsan velit nisl et gravida augue accumsan lobortis. Pharetra sed sed metus eu at tristique in fermentum lacus. Facilisis mattis adipiscing interdum venenatis donec elementum id justo. Elementum nullam sed felis in massa sem volutpat sit. Ut aliquam enim at arcu aliquam nam adipiscing. Ornare amet volutpat eget rhoncus pellentesque consectetur eget purus at. Vulputate morbi et at velit sapien.
+    `,
+  },
+  {
+    id: 8,
+    question: 'How do people live with drug users?',
+    answer: `
+    Lorem ipsum dolor sit amet consectetur. Pretium est at consectetur odio neque lobortis justo eu fusce. Mi ipsum in interdum ut scelerisque amet elit ut. Scelerisque facilisis non orci metus ornare. At luctus eu morbi sollicitudin amet in elementum accumsan. Sed mollis sagittis eleifend gravida. Feugiat feugiat facilisis pellentesque consequat placerat etiam vitae turpis. Cras nunc massa gravida potenti. In euismod in senectus dictum quam quam urna ac. Sem vel faucibus morbi elementum urna.
+    Vitae ipsum malesuada malesuada blandit metus mauris purus leo. Facilisi porttitor dolor amet adipiscing ultricies nunc in. Lacinia tempus placerat lorem aliquam. Eu sed quis pellentesque semper at tellus. Ut libero a iaculis facilisis quis lectus libero amet. Volutpat amet accumsan velit nisl et gravida augue accumsan lobortis. Pharetra sed sed metus eu at tristique in fermentum lacus. Facilisis mattis adipiscing interdum venenatis donec elementum id justo. Elementum nullam sed felis in massa sem volutpat sit. Ut aliquam enim at arcu aliquam nam adipiscing. Ornare amet volutpat eget rhoncus pellentesque consectetur eget purus at. Vulputate morbi et at velit sapien.
+    `,
+  },
+];
+
+export default faqs;
