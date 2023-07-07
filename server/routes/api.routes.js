5c982694b4672a337fc81aaebf3c8e87d733d6f6
duanedave <duanedave17@gmail.com>
2023-07-07 16:01:30 +0100

    Render routes

--- server/routes/api.routes.js
+++ server/routes/api.routes.js
@@ -2,10 +2,14 @@ import { Router } from 'express';
 
 import userRouter from './user.routes.js';
 import newsletterRouter from './newsletter.routes.js';
+import courseRouter from './course.routes.js';
+import commentRouter from './comment.routes.js';
 
 const apiRouter = Router();
 
 apiRouter.use('/user', userRouter);
 apiRouter.use('/newsletter', newsletterRouter);
+apiRouter.use('/course', courseRouter);
+apiRouter.use('/comment', commentRouter);
 
 export default apiRouter;
