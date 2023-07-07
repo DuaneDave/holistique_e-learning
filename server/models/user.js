b0e7600e0c8a317a3a9e586fc1ea1189edb5b2d6
duanedave <duanedave17@gmail.com>
2023-07-07 16:01:53 +0100

    Update the use model

--- server/models/user.js
+++ server/models/user.js
@@ -11,12 +11,19 @@ const userSchema = new Schema(
     email: {
       type: String,
       unique: true,
+      trim: true,
     },
     password: {
       type: String,
+      trim: true,
       minLength: [8, 'Password must be at least 8 characters long'],
       required: [true, 'Password is required'],
     },
+    firstName: { type: String, trim: true },
+    lastName: { type: String, trim: true },
+    profile_image: { type: String },
+    phone: { type: String, trim: true },
+    courses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
   },
   {
     timestamps: true,
