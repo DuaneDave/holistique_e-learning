637a08759d48277a1d65737c64377a6cb4086f0c
duanedave <duanedave17@gmail.com>
2023-07-07 16:01:41 +0100

    Update the user route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
 
-import { signup, login, logout } from '../controllers/user.controller.js';
+import { signup, login, logout, editProfile, addCourse } from '../controllers/user.controller.js';
 
+const upload = multer({ dest: 'uploads/' });
 const userRouter = Router();
 
 userRouter.post('/signup', signup);
 userRouter.post('/login', login);
 userRouter.delete('/logout', logout);
+userRouter.put('/edit-profile', upload.single('file'), editProfile);
+userRouter.put('/add-course/:id', addCourse);
 
 
 export default userRouter;
