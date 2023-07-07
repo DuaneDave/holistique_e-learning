cc093a322ca2fbccdff6607761183a575e628297
duanedave <duanedave17@gmail.com>
2023-07-07 16:00:54 +0100

    Add user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/user.js';
+import Course from '../models/course.js';
 
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET;
@@ -81,7 +82,75 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  console.log('logout');
-
   res.clearCookie('token').json({ message: 'User logged out successfully' });
-}
+};
+
+export const editProfile = async (req, res) => {
+  const { id } = req.params;
+  const { email, firstName, lastName, phone } = req.body;
+
+  try {
+    const foundUser = await User.findById(id);
+    let newPath = null;
+
+    if (req.file) {
+      const { originalname, path } = req.file;
+      const parts = originalname.split('.');
+      const ext = parts[parts.length - 1];
+      newPath = path + '.' + ext;
+      fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+
+    jwt.verify(token, secret, {}, async (err, decoded) => {
+      if (err) throw err;
+
+      await foundUser.updateOne({
+        email,
+        firstName,
+        lastName,
+        phone,
+        profile_image: newPath,
+      });
+
+      res.status(200).json({ message: 'User updated successfully' });
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message, status: 500 });
+  }
+};
+
+export const addCourse = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const { token } = req.cookies;
+
+    jwt.verify(token, secret, {}, async (err, decoded) => {
+      if (err) throw err;
+
+      const foundCourse = await Course.findById(id);
+
+      if (decoded.courses.includes(foundCourse._id)) {
+        return res.status(400).json({
+          message: 'You already added this course',
+          status: 400,
+        });
+      }
+
+      await User.findByIdAndUpdate(
+        decoded.id,
+        { $push: { courses: foundCourse._id } },
+        { new: true }
+      );
+
+      res.status(200).json({
+        message: 'Course added successfully',
+        status: 200,
+      });
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message, status: 500 });
+  }
+};
