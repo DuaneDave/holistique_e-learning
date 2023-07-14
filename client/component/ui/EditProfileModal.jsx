232cfe66d885820c233bb04bd1a3992db2ef4f64
Lena <summyalena@gmail.com>
2023-07-14 17:20:44 +0100

    Restyle edit profile modal

--- client/component/ui/EditProfileModal.jsx
+++ client/component/ui/EditProfileModal.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 
 import arrow from "../../public/assets/icons/arrowleft (2).svg";
 import sign from "../../public/assets/icons/material-symbols_done.png";
-import avatar from "../../public/assets/icons/avatar.png";
+import avatar from "../../public/assets/icons/emptyavatar.png";
 import upload from "../../public/assets/icons/Vector (1).svg";
 import styles from "./ui.module.css";
 
 import { AuthContext } from "@/store/authContext";
 
-function ProfileModal({ openmodal, setopenmodal, setshow }) {
-  const { logout, user } = useContext(AuthContext);
+function EditProfileModal({setopenmodal }) {
+  const { logout, user, setEditProfileVisibility } = useContext(AuthContext);
   const [message, setMessage] = useState("");
   const [image, setImage] = useState({preview: "", raw: ""});
 
@@ -24,12 +24,6 @@ function ProfileModal({ openmodal, setopenmodal, setshow }) {
   }
   const [profileData, setProfileData] = useState(data);
 
-  const handleProfileCLose = () => {
-    setopenmodal(false);
-    setshow(false);
-    console.log(openmodal);
-  };
-
   const handleInput = (e) => {
     const { name, value } = e.target;
     setProfileData({ ...profileData, [name]: value });
@@ -37,7 +31,6 @@ function ProfileModal({ openmodal, setopenmodal, setshow }) {
 
 
   const handleImageChange = (e) => {
-     console.log('hello');
      if(e.target.files.length){
      setImage({
       preview: URL.createObjectURL(e.target.files[0]),
@@ -45,6 +38,19 @@ function ProfileModal({ openmodal, setopenmodal, setshow }) {
      })
     }
   }
+
+  useEffect(()=> {
+     const fetchUser = async() => {
+        try{
+          const response = await fetch(`http://localhost:4000/api/users/${user.id}`).
+          then((res)=> res.data);
+          console.log(response);
+        } catch(err){
+          console.log(err);
+        }
+     }
+     fetchUser();
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -73,21 +79,23 @@ function ProfileModal({ openmodal, setopenmodal, setshow }) {
 
   return (
     <div className={`flex center flex-col ${styles.profilecontainer}`}>
-      <div className="flex flex-col center">
-        <form onSubmit={handleSubmit} className="flex flex-col">
+      <div className="flex flex-col full-width center">
+        <form onSubmit={handleSubmit} className="full-width flex flex-col">
           <div className="flex flex-col center">
             <div className={`flex ${styles.profileheading}`}>
-              <Image src={arrow} alt="arrow" onClick={handleProfileCLose}/>
-              <h2>Edit Profile</h2>
+              <button className={`flex center ${styles.backarrowbutton}`} aria-label='exit profile edit'>
+              <Image className={`${styles.backarrow}`} src={arrow} alt="exit profile edit" onClick={() => setopenmodal(false)}/>
+              </button>
+              <h2 className='flex'>Edit Profile</h2>
               <button type="submit" className={styles.checkoutprofile}>
-                <Image className={styles.goodsign} src={sign} alt="good_sign" />
+                <Image className={` ${styles.checkoutimage}`} src={sign} alt="submit profile info" />
               </button>
             </div>
 
             <div className={styles.uploadImages}>
               <div>
              {image.preview ? (<>
-             <Image styles={`flex circle ${styles.uploadimage}`} src={image.preview} alt='preview' width={80} height={80} /><button className={styles.uploadbutton} type="button">
+             <Image styles={`flex circle ${styles.uploadimage}`} src={image.preview} alt='preview' width={90} height={90} /><button className={styles.uploadbutton} type="button">
                   <label htmlFor="upload-button">
                     <Image
                       className={`flex circle ${styles.upload}`}
@@ -99,12 +107,13 @@ function ProfileModal({ openmodal, setopenmodal, setshow }) {
               (
               <>
             
-              <Image className={styles.avatar} src={avatar} alt="avatar" />
+              <Image className={styles.avatar} src={avatar} width={100} height={100} alt="avatar" />
               <button className={styles.uploadbutton} type="button">
             <label htmlFor="upload-button">
                   <Image
                     className={`flex circle ${styles.upload}`}
                     src={upload}
+               
                     alt="upload-image" />
               </label>
 
@@ -187,4 +196,4 @@ function ProfileModal({ openmodal, setopenmodal, setshow }) {
   );
 }
 
-export default ProfileModal;
+export default EditProfileModal;
