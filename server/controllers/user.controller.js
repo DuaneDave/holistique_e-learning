7a14c8c03f6070cf082944d31c407c99d067beb8
Lena <summyalena@gmail.com>
2023-07-16 18:03:54 +0100

    Add put function for profile modal

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,8 +1,8 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-import User from '../models/user.js';
-import Course from '../models/course.js';
+import User from "../models/user.js";
+import Course from "../models/course.js";
 
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET;
@@ -17,7 +17,7 @@ export const signup = async (req, res) => {
       console.log(isExisting);
       return res
         .status(400)
-        .json({ message: 'User already exists', status: 400 });
+        .json({ message: "User already exists", status: 400 });
     }
 
     User.create({
@@ -26,7 +26,7 @@ export const signup = async (req, res) => {
       password: bcrypt.hashSync(password, salt),
     });
 
-    res.status(201).json({ message: 'User created successfully', status: 201 });
+    res.status(201).json({ message: "User created successfully", status: 201 });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -41,7 +41,7 @@ export const login = async (req, res) => {
     if (!foundUser) {
       return res
         .status(400)
-        .json({ message: 'Invalid credentials', status: 400 });
+        .json({ message: "Invalid credentials", status: 400 });
     }
 
     const isPasswordCorrect = bcrypt.compareSync(password, foundUser.password);
@@ -49,7 +49,7 @@ export const login = async (req, res) => {
     if (!isPasswordCorrect) {
       return res
         .status(400)
-        .json({ message: 'Invalid credentials', status: 400 });
+        .json({ message: "Invalid credentials", status: 400 });
     }
 
     jwt.sign(
@@ -62,11 +62,11 @@ export const login = async (req, res) => {
           return res.status(500).json({ message: error.message, status: 500 });
         }
 
-        res.cookie('token', token).json({
+        res.cookie("token", token).json({
           id: foundUser._id,
           username: foundUser.username,
           email: foundUser.email,
-          message: 'User logged in successfully',
+          message: "User logged in successfully",
         });
       }
     );
@@ -76,7 +76,7 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.clearCookie('token').json({ message: 'User logged out successfully' });
+  res.clearCookie("token").json({ message: "User logged out successfully" });
 };
 
 export const editProfile = async (req, res) => {
@@ -89,9 +89,9 @@ export const editProfile = async (req, res) => {
 
     if (req.file) {
       const { originalname, path } = req.file;
-      const parts = originalname.split('.');
+      const parts = originalname.split(".");
       const ext = parts[parts.length - 1];
-      newPath = path + '.' + ext;
+      newPath = path + "." + ext;
       fs.renameSync(path, newPath);
     }
 
@@ -108,7 +108,7 @@ export const editProfile = async (req, res) => {
         profile_image: newPath,
       });
 
-      res.status(200).json({ message: 'User updated successfully' });
+      res.status(200).json({ message: "User updated successfully" });
     });
   } catch (error) {
     res.status(500).json({ message: error.message, status: 500 });
@@ -128,7 +128,7 @@ export const addCourse = async (req, res) => {
 
       if (decoded.courses.includes(foundCourse._id)) {
         return res.status(400).json({
-          message: 'You already added this course',
+          message: "You already added this course",
           status: 400,
         });
       }
@@ -140,7 +140,7 @@ export const addCourse = async (req, res) => {
       );
 
       res.status(200).json({
-        message: 'Course added successfully',
+        message: "Course added successfully",
         status: 200,
       });
     });
@@ -150,12 +150,25 @@ export const addCourse = async (req, res) => {
 };
 
 export const getUser = async (req, res) => {
-  const { id } = req.params;
+  const { token } = req.cookies;
 
   try {
-    const foundUser = await User.findById(id).populate('courses');
+    jwt.verify(token, secret, {}, async (err, decoded) => {
+      if (err) throw err;
 
-    res.status(200).json({ user: foundUser, status: 200 });
+      res
+        .status(200)
+        .json({
+          id: decoded.id,
+          username: decoded.username,
+          email: decoded.email,
+          firstName: decoded?.firstName,
+          lastName: decoded?.lastName,
+          phone: decoded?.phone,
+          profile_image: decoded?.profile_image,
+          status: 200,
+        });
+    });
   } catch (error) {
     res.status(500).json({ message: error.message, status: 500 });
   }
