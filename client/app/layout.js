835e7ca5a0a46454e6251d68c2532290a065a8bc
duanedave <duanedave17@gmail.com>
2023-07-25 01:04:00 +0100

    Comment tawk

--- client/app/layout.js
+++ client/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Tawk />
+        {/* <Tawk /> */}
       </head>
       <body className={inter.className}>
         <AuthProvider>
