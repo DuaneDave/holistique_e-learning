06a22b77f595fb735f4a1a258aef20e07e3d30b9
duanedave <duanedave17@gmail.com>
2023-07-28 22:29:22 +0100

    Push to production

--- server/config/origin.js
+++ server/config/origin.js
@@ -1,15 +1,14 @@
 const allowedOrigins = () => {
+  // const origins = {
+  //   origin: 'http://localhost:3000',
+  //   credentials: true,
+  // };
+
   const origins = {
-    origin: 'http://localhost:3000',
+    origin:
+      'https://holistique-e-learning-git-resourcecenter-duanedave.vercel.app/',
     credentials: true,
   };
-
-  if (process.env.NODE_ENV === 'production') {
-    return {
-      origin: 'https://holistique-e-learning-git-resourcecenter-duanedave.vercel.app/',
-      credentials: true,
-    };
-  }
 
   return origins;
 };
