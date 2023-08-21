e6f8a275e9c0a8da6982fb0a5589a44447194681
duanedave <duanedave17@gmail.com>
2023-08-21 01:41:51 +0100

    Add static server

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-import db from './config/db.js';
 import allowedOrigins from './config/origin.js';
 import apiRouter from './routes/api.routes.js';
-import multer from 'multer';
-
-db();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = 4000;
 
 app.use(cors(allowedOrigins()));
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cookieParser());
 
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', apiRouter);
 
 app.listen(PORT, () => {
